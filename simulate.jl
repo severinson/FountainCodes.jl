3da93c0ab4991c31b6d8755d6a9a7b3a5ce59f80
Albin Severinson <albin@severinson.org>
2018-02-20 14:04:14 +0100

    renamed

--- simulate.jl
+++ simulate.jl
@@ -1,66 +1,48 @@
-using RaptorCodes, DataStructures, DataFrames, CSV
+include("simulate.jl")
 
-struct Simulation
-    p::RaptorCodes.Parameters
-    overhead::Int # absolute reception overhead
-    C::Array{RaptorCodes.ISymbol,1}
-    n::Int # number of samples
-end
+""" Packages
+- DataFrames
+- CSV
+"""
 
-function repr(r::Simulation)
-    return "Simulation($(RaptorCodes.repr(r.p)), $(r.overhead))"
-end
-
-function init(p::R10Parameters)
-    C = Array{RaptorCodes.ISymbol,1}(p.L)
-    for i = 1:p.K
-        C[i] = RaptorCodes.ISymbol(i, Set([i]))
-    end
-    RaptorCodes.r10_ldpc_encode!(C, p)
-    RaptorCodes.r10_hdpc_encode!(C, p)
-    return C
-end
-
-function init(p::LTParameters)
-    C = Array{RaptorCodes.ISymbol,1}(p.L)
-    for i = 1:p.K
-        C[i] = RaptorCodes.ISymbol(i, Set([i]))
-    end
-    return C
-end
-
-function sample(sr::Simulation)
-    d = RaptorCodes.Decoder(sr.p)
-    for _ in 1:(sr.p.K+sr.overhead)
-        X = rand(1:(2<<16))
-        s = RaptorCodes.lt_generate(sr.C, X, sr.p)
-        RaptorCodes.add!(d, s)
-    end
-    RaptorCodes.decode!(d, false)
-    return d.metrics
-end
-
-function simulate(sr::Simulation, dct::Dict, dir::String)
-    mkpath(dir)
-    filename = joinpath(
-        dir,
-        "$(repr(sr)).csv"
-    )
-    try
-        return CSV.read(filename)
-    catch
-    end
-    samples = DefaultDict(Vector)
-    for i in 1:sr.n
-        for v in dct
-            push!(samples[v[1]], v[2])
+function r10_main()
+    for K in 1000:500:8000
+        p = R10Parameters(K)
+        C = init(p)
+        for overhead in 0:50:500
+            sr = Simulation(p, overhead, C, 100)
+            println(repr(sr))
+            df = simulate(sr, "results")
         end
-        for v in sample(sr)
-            push!(samples[v[1]], v[2])
-        end
-        println(samples)
     end
-    df = DataFrame(samples)
-    CSV.write(filename, df)
-    return df
 end
+
+function lt_main()
+    futures = Array{Future,1}()
+    for K in 1000:500:8000
+        mode = K-1
+        delta = 0.0001
+        s = RaptorCodes.Soliton(K, mode, delta)
+        p = LTParameters(K, s)
+        C = init(p)
+        for overhead in 0:50:500
+            sr = Simulation(p, overhead, C, 100)
+            println(repr(sr))
+            # f = @spawn simulate(sr, "results")
+            dct = Dict()
+            dct["K"] = K
+            dct["overhead"] = overhead
+            dct["mode"] = mode
+            dct["delta"] = delta
+            df = simulate(sr, dct, "results2")
+            # push!(futures, f)
+        end
+    end
+    # dfs = Array{DataFrame,1}()
+    # for f in futures
+    #     df = fetch
+    #     push!(dfs, fetch(f))
+    # end
+end
+
+lt_main()
