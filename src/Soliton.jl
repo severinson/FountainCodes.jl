19c45d63c39b189832217aafb63b1ac1be02441b
Albin Severinson <albin@severinson.org>
2018-03-13 16:34:11 +0100

    less strict argument types

--- src/Soliton.jl
+++ src/Soliton.jl
@@ -10,7 +10,7 @@ struct Soliton <: Sampleable{Univariate, Discrete}
     R::Float64
     c::Float64
     beta::Float64 # normalization constant
-    function Soliton(K::Int64, mode::Int64, delta::Float64)
+    function Soliton(K::Int64, mode::Int, delta::Real)
         if !(0 < delta < 1)
             error("delta must be 0 < delta < 1")
         end
@@ -37,7 +37,7 @@ end
 Base.repr(s::Soliton) = "Soliton($(s.K), $(s.mode), $(s.delta))"
 
 "Robust component of the Soliton distribution."
-function tau(K::Int64, mode::Int64, delta::Float64, R::Float64, i::Int64)
+function tau(K::Int64, mode::Int, delta::Float64, R::Float64, i::Int64)
     if i < mode
         return 1 / (i * mode)
     elseif i == mode
@@ -50,7 +50,7 @@ function tau(K::Int64, mode::Int64, delta::Float64, R::Float64, i::Int64)
 end
 
 "Robust component of the Soliton distribution."
-function tau(f::Soliton, i::Int64)
+function tau(f::Soliton, i::Int)
     if i < f.mode
         return 1 / (i * f.mode)
     elseif i == f.mode
@@ -63,7 +63,7 @@ function tau(f::Soliton, i::Int64)
 end
 
 "Ideal component of the Soliton distribution."
-function rho(K::Int64, i::Int64)
+function rho(K::Int64, i::Int)
     if i == 1
         return 1 / K
     elseif i <= K
@@ -74,7 +74,7 @@ function rho(K::Int64, i::Int64)
 end
 
 "Ideal component of the Soliton distribution."
-function rho(f::Soliton, i::Int64)
+function rho(f::Soliton, i::Int)
     if i == 1
         return 1 / f.K
     elseif i <= f.K
@@ -89,12 +89,12 @@ function params(d::Soliton)
 end
 
 "Soliton distribution probability density function."
-function pdf(f::Soliton, i::Int64)
+function pdf(f::Soliton, i::Int)
     return (tau(f, i) + rho(f, i)) / f.beta
 end
 
 "Soliton distribution cumulative density function."
-function cdf(f::Soliton, i::Int64)
+function cdf(f::Soliton, i::Int)
     return sum(pdf(f, j) for j=1:i)
 end
 
