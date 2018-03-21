3abcf6eed081de818ae520da66596d1553143507
Albin Severinson <albin@severinson.org>
2018-03-21 14:07:09 +0100

    LTQ benchmark

--- benchmark.jl
+++ benchmark.jl
@@ -4,47 +4,34 @@ using RaptorCodes
 using ProfileView
 
 function init_decoder(
-    p::R10Parameters,
+    p::Code,
     C::Vector{Vector{GF256}},
     r::Int, n::Int) :: RaptorCodes.Decoder
     d = RaptorCodes.Decoder(p)
     for j in 1:p.K+r
-        s = RaptorCodes.lt_generate(C, (n-1)*(p.K)+j, p)
+        s = RaptorCodes.ltgenerate(C, (n-1)*(p.K)+j, p)
         RaptorCodes.add!(d, s)
     end
     return d
 end
 
-# function init_gf256(
-#     p::QLTParameters,
-#     C::Vector{RaptorCodes.ISymbol{R10Value}},
-#     r::Int, n::Int) :: RaptorCodes.Decoder
-#     dd = RaptorCodes.Soliton(k, Int(round(k*2/3)), 0.01)
-#     p = RaptorCodes.QLTParameters(k, dd)
-#     d = RaptorCodes.Decoder(p)
-#     C = Vector{Vector{GF256}}(p.L)
-#     for i = 1:p.K
-#         C[i] = Vector{GF256}([i % 256])
-#     end
-#     return p, d, C
-# end
-
 function encode_benchmark(
-    p::R10Parameters,
+    p::Code,
     C::Vector{Vector{GF256}},
     r::Int, n::Int)
     for i in 1:n
-        RaptorCodes.r10_ldpc_encode!(C, p)
-        RaptorCodes.r10_hdpc_encode!(C, p)
+        precode!(C, p)
         for j in 1:p.K+r
-            _ = RaptorCodes.lt_generate(C, (i-1)*(p.K)+j, p)
+            _ = RaptorCodes.ltgenerate(C, (i-1)*(p.K)+j, p)
         end
     end
 end
 
 function main(K=1000, r=500, n=10)
     Profile.clear()
-    p = R10Parameters(K)
+    # p = R10Parameters(K)
+    dd = RaptorCodes.Soliton(K, Int(round(K*2/3)), 0.01)
+    p = RaptorCodes.QLTParameters(K, dd)
     C = Vector{Vector{GF256}}(p.L)
     for i = 1:p.K
         C[i] = Vector{GF256}([i % 256])
@@ -70,7 +57,7 @@ function main(K=1000, r=500, n=10)
     end
     @printf "%.3fGbps\n" 256*8*p.K/(td/n)/1000/1000/1000
 
-    ProfileView.view()
+    # ProfileView.view()
 end
 
 main()
