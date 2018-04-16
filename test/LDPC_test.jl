ebe249fc5cfb0a49e01e94197f0d118fd4d0f307
Albin Severinson <albin@severinson.org>
2018-04-16 22:11:15 +0200

    LDPC code test

--- test/LDPC_test.jl
+++ test/LDPC_test.jl
@@ -8,7 +8,6 @@ function test_ldpc_decoder_1()
     H[2, 4] = true
     H[3, 5] = true
     H[3, 6] = true
-    println(H)
     c = LDPC10{GF256}(H)
     c.erased[1] = true
     c.erased[3] = true
@@ -25,4 +24,18 @@ function test_ldpc_decoder_1()
 end
 @test test_ldpc_decoder_1()
 
-
+function test_ldpc_decoder_2()
+    H = Matrix{Bool}(readdlm("./test/H_612_1224.txt"))
+    c = LDPC10{GF256}(H)
+    I = [rand(1:c.n) for _ in 1:100]
+    c.erased[I] = true
+    c.Y[I] = 1 # make sure these get reset to 0 during decoding
+    d = Decoder(c)
+    decode!(d)
+    get_source!(view(c.Y, c.erased), d)
+    if !iszero(c.Y)
+        error("LDPC decoding failed")
+    end
+    return true
+end
+@test test_ldpc_decoder_2()
