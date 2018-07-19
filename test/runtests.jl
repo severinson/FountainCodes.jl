f71bd4b0c58457b6fd61d2602a8a9cbe081ce7e6
Albin Severinson <albin@severinson.org>
2018-07-19 18:12:26 +0200

    disjoint sets implrmentation that tracks component size

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ println("Starting tests")
 @time @testset "Numerical function inversion" begin include("Numinv_test.jl") end
 @time @testset "Finite field arithmetic" begin include("Arithmetic_test.jl") end
 @time @testset "Soliton distribution" begin include("Soliton_test.jl") end
+@time @testset "IntDisjointSets" begin include("DisjointSet_test.jl") end
 @time @testset "Gray sequence" begin include("gray_test.jl") end
 @time @testset "R10 encoder" begin include("R10Encode_test.jl") end
 @time @testset "Raptor decoder" begin include("decoder_test.jl") end
