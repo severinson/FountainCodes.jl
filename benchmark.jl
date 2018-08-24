90062c949ce8df00261852413e2ae9dcfdaf05ac
Albin Severinson <albin@severinson.org>
2018-08-24 16:42:38 +0200

    print number of inactivations

--- benchmark.jl
+++ benchmark.jl
@@ -8,6 +8,7 @@ function init_decoder(
     C::Vector{Vector{GF256}},
     r::Int, n::Int) :: RaptorCodes.Decoder
     d = RaptorCodes.Decoder(p)
+    println("n = $n")
     for j in 1:p.K+r
         s = RaptorCodes.ltgenerate(C, (n-1)*(p.K)+j, p)
         RaptorCodes.add!(d, s)
@@ -27,11 +28,11 @@ function encode_benchmark(
     end
 end
 
-function main(K=1000, r=500, n=100)
+function main(K=1000, r=20, n=100)
     Profile.clear()
-    p = R10(K)
-    # dd = Soliton(K, Int(round(K*2/3)), 0.01)
-    # p = RaptorCodes.LTQ(K, dd)
+    p = RQ(K)
+    # dd = Soliton(K, Int(round(K*2/3)), 0.1)
+    # p = RaptorCodes.LT(K, dd)
     C = Vector{Vector{GF256}}(p.L)
     for i = 1:p.K
         C[i] = Vector{GF256}([i % 256])
@@ -51,12 +52,16 @@ function main(K=1000, r=500, n=100)
     d = init_decoder(p, C, r, 1)
     @timev RaptorCodes.decode!(d)
     td = 0.0
+    inactivations = 0.0
     for i in 1:n
         d = init_decoder(p, C, r, i)
         td += @elapsed @profile RaptorCodes.decode!(d)
-        println(d.metrics)
+        inactivations += d.metrics["inactivations"]
+        # println(d.metrics)
     end
     @printf "%.3fGbps\n" 256*8*p.K/(td/n)/1000/1000/1000
+    inactivations /= n
+    println("$inactivations inactivations")
 
     # Profile.print()
     ProfileView.view()
