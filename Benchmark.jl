c8ee9354e809cc5d7b66dc9657d2c65cca4c55aa
Albin Severinson <albin@severinson.org>
2020-05-19 14:42:32 +0200

    Updated benchmark.

--- Benchmark.jl
+++ Benchmark.jl
@@ -1,49 +1,33 @@
 using FountainCodes
 
-# R10 benchmarks
-function r10_sample(r10, src, r)
-    d = Decoder(r10)
-    for _ in 1:r
-        s = ltgenerate(src, rand(0:8192), r10)
-        add!(d, s.neighbours, s.value)
-    end
-    decode!(d)
-    return
-end
-
 function benchmark_r10(K=1000, r=1200, m=256, n=100)
     r10 = R10(K)
-    src = [zeros(GF256, m) for _ in 1:r10.L]
-    precode!(src, r10)
-    r10_sample(r10, src, r) # warmup
-    total = 0.0
+    d = Decoder(r10)
+    src = [GF256(i % 256) for i in 1:K]
+    inter = precode(src, r10)
+    Vs = [zero(inter[1]) for _ in 1:(r10.S+r10.H)] # parity symbol values
+    append!(Vs, [get_value(r10, X, inter) for X in 0:r-1]) # LT symbol values
+    Xs = -(r10.S+r10.H):r-1 # ESIs incl. parity symbols
+
+    t = 0.0
     for _ in 1:n
-        total += @elapsed r10_sample(r10, src, r)
+        t += @elapsed decode(r10, Xs, Vs)
     end
-    total /= n
-    return total
+    t /= n
+    println(r10)
+    println("Decoding time: $t s")
 end
 
-# RQ benchmarks
-function rq_sample(rq, src, r)
-    d = Decoder(rq)
-    for _ in 1:r
-        s = ltgenerate(src, rand(0:65000), rq)
-        add!(d, s.neighbours, s.value)
-    end
-    decode!(d)
-    return
-end
-
-function benchmark_rq(K=1000, r=1200, m=256, n=100)
-    rq = RQ(K)
-    src = [zeros(GF256, m) for _ in 1:rq.L]
-    precode!(src, rq)
-    rq_sample(rq, src, r) # warmup
-    total = 0.0
+function benchmark_lt(K=1000, r=1300, m=256, n=100, M=K-1, δ=1e-6)
+    dd = Soliton(K, M, δ)
+    lt = LT(K, dd)
+    src = [Vector{GF256}([i % 256]) for i in 1:K]
+    Vs = [get_value(lt, X, src) for X in 1:r]
+    t = 0.0
     for _ in 1:n
-        total += @elapsed rq_sample(rq, src, r)
+        t += @elapsed decode(lt, 1:r, Vs)
     end
-    total /= n
-    return total
+    t /= n
+    println(lt)
+    println("Decoding time: $t s")
 end
