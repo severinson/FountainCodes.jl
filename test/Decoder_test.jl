74bc36880a3b7768c494d1c4989fb50fc76f3a7e
Albin Severinson <albin@severinson.org>
2021-07-27 14:41:47 +0200

    Added tests for bidiagonal and tridiagonal generator matrices

--- test/Decoder_test.jl
+++ test/Decoder_test.jl
@@ -19,13 +19,56 @@ function test_diagonal(CT, K::Integer)
     if decoder.metrics["inactivations"] != 0
         error("expected 0 inactivations, but got $(decoder.metrics["inactivations"])")
     end
-    return true
+    true
 end
-for K in [1, 10, 100, 200, 250, 254]
-    @test test_diagonal(Bool, K)
-end
+# for K in [1, 10, 100, 200, 250, 254]
+#     @test test_diagonal(Bool, K)
+# end
 for K in [1, 10, 100, 200, 250, 254]
     @test test_diagonal(GF256, K)
+end
+
+function test_bidiagonal(K::Integer, Tv=GF256)
+    dv = ones(Tv, K)
+    ev = ones(Tv, K-1)
+    A = sparse(Bidiagonal(dv, ev, :U))
+    Vs = [GF256(i%256) for i in 0:K-1]
+    decoder = Decoder(A)
+    dec = decode(A, Vs; decoder)
+    for i in 1:K
+        if dec[i] != Vs[i]
+            error("expected $(Vs[i]), but got $(dec[i])")
+        end
+    end
+    if decoder.metrics["inactivations"] != 0
+        error("expected 0 inactivations, but got $(decoder.metrics["inactivations"])")
+    end        
+    true
+end
+for K in [10, 100, 200, 250, 254]
+    @test test_bidiagonal(K)
+end
+
+function test_tridiagonal(K::Integer, Tv=GF256)
+    du = ones(Tv, K-1)
+    d = ones(Tv, K)
+    dl = ones(Tv, K-1)
+    A = sparse(Tridiagonal(dl, d, du))
+    Vs = [GF256(i%256) for i in 0:K-1]
+    decoder = Decoder(A)
+    dec = decode(A, Vs; decoder)
+    for i in 1:K
+        if dec[i] != Vs[i]
+            error("expected $(Vs[i]), but got $(dec[i])")
+        end
+    end
+    if decoder.metrics["inactivations"] != 1
+        error("expected 1 inactivation, but got $(decoder.metrics["inactivations"])")
+    end        
+    true
+end
+for K in [10, 100, 200, 250, 254]
+    @test test_tridiagonal(K)
 end
 
 """Test decoding for a dense constraint matrix."""
@@ -48,7 +91,7 @@ function test_dense(K::Integer)
     if decoder.metrics["inactivations"] != K-1
         error("expected $(K-1) inactivations, but got $(decoder.metrics["inactivations"])")
     end
-    return true
+    true
 end
 for K in [1, 10, 100, 200, 250, 254]
     @test test_dense(K)
