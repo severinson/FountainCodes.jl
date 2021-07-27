da3065e3c16d36cfb0a3f78b56d2ff0df09e67e6
Albin Severinson <albin@severinson.org>
2021-07-27 13:58:42 +0200

    Cleanup, do not allow arithmetic between GF256 elements and regular integers

--- src/GF256.jl
+++ src/GF256.jl
@@ -3,73 +3,93 @@ export GF256
 """
     struct GF256
 
-Element of GF256 with arithmetic as defined in rfc6330.
+Element of a finite field of size 256 with arithmetic as defined in rfc6330.
 
 """
 struct GF256 <: Integer
     x::UInt8
-    function GF256(x)
-        return new(UInt8(x))
-    end
 end
+
+GF256(x::Integer) = GF256(UInt8(x))
+GF256(a::GF256) = GF256(a.x)
 
 function show(io::IO, a::GF256)
     print(io, "GF256($(a.x))")
 end
 
-Base.convert(::Type{Bool}, a::GF256) = Bool(a.x)
-Base.convert(::Type{Int}, a::GF256) = Int(a.x)
-Base.convert(::Type{GF256}, x::Int) = GF256(x)
-Base.convert(::Type{GF256}, x::UInt8) = GF256(x)
-Base.convert(::Type{GF256}, x::Bool) = x ? GF256(1) : GF256(0)
+# TODO: remove
+# Base.convert(::Type{Bool}, a::GF256) = Bool(a.x)
+# Base.convert(::Type{Int}, a::GF256) = Int(a.x)
+# Base.convert(::Type{GF256}, x::Int) = GF256(x)
+# Base.convert(::Type{GF256}, x::UInt8) = GF256(x)
+# Base.convert(::Type{GF256}, x::Bool) = x ? GF256(1) : GF256(0)
+# Base.promote_rule(::Type{GF256}, ::Type{Bool}) = GF256
+# Base.promote_rule(::Type{GF256}, ::Type{Int}) = Int
 
-Base.promote_rule(::Type{GF256}, ::Type{Bool}) = GF256
-Base.promote_rule(::Type{GF256}, ::Type{Int}) = Int
-
-Base.zero(a::GF256) = GF256(0)
+# zeros/ones
+Base.zero(::GF256) = GF256(0)
 Base.zero(::Type{GF256}) = GF256(0)
-Base.one(a::GF256) = GF256(1)
+Base.one(::GF256) = GF256(1)
 Base.one(::Type{GF256}) = GF256(1)
 Base.iszero(a::GF256) = iszero(a.x)
-
-Base.length(a::GF256) = 1
-Base.iterate(a::GF256) = (a, nothing)
-Base.iterate(a::GF256, ::Nothing) = nothing
-
-Base.rand(::Type{GF256}) = GF256(rand(UInt8))
+Base.isone(a::GF256) = isone(a.x)
 
 function Base.zeros(::Type{GF256}, dims::Vararg{Union{Int, AbstractUnitRange},N} where N)
     rv = Array{GF256}(undef, dims...)
     rv .= zero(GF256)
-    return rv
 end
 
 function Base.ones(::Type{GF256}, dims::Vararg{Union{Int, AbstractUnitRange},N} where N)
     rv = Array{GF256}(undef, dims...)
     rv .= one(GF256)
-    return rv
 end
 
+# iteration
+Base.length(a::GF256) = 1
+Base.iterate(a::GF256) = (a, nothing)
+Base.iterate(a::GF256, ::Nothing) = nothing
+
+# random numbers
+Base.rand(rng::AbstractRNG, ::Type{GF256}) = GF256(rand(rng, UInt8))
+
 """logarithm as defined by rfc6330. used for multiplication and division."""
-function logrq(a::GF256)::Int
-    return iszero(a) ? throw(DomainError(0, "logrq(0) undefined")) : RQ_OCT_LOG[a.x]
+function Base.log(a::GF256)::GF256
+    !iszero(a) || throw(DomainError(GF256(0), "log(GF256(0)) is undefined"))
+    GF256(RQ_OCT_LOG[a.x])
 end
 
 """exponentiation as defined by rfc6330. used for multiplication and division."""
-function exprq(a::Int)::GF256
-    return RQ_OCT_EXP[a+1]
+function Base.exp(a::GF256)::GF256
+    a.x != 255 || throw(DomainError(GF256(255)), "exp(GF256(255)) is undefined")
+    RQ_OCT_EXP[a.x+1]
 end
 
+# TODO: remove
+# Base.:+(a::Bool, b::GF256) = +(promote(a, b)...)
+# Base.:-(a::Bool, b::GF256) = -(promote(a, b)...)
+# Base.:+(a::GF256, b::Bool) = +(promote(a, b)...)
+# Base.:-(a::GF256, b::Bool) = -(promote(a, b)...)
+# Base.:/(a::GF256, b::Bool) = /(promote(a, b)...)
+# Base.:*(a::Bool, b::GF256) = a ? b : zero(GF256)
+# Base.:*(a::GF256, b::Bool) = b*a
+
+# TODO: faulty implementation
+# Base.:^(a::GF256, b::Integer) = b < 0 ? throw(DomainError(b, "^$b undefined")) : exprq(b * logrq(a) % 510)
+
 # arithmetic
-Base.:+(a::Bool, b::GF256) = +(promote(a, b)...)
-Base.:-(a::Bool, b::GF256) = -(promote(a, b)...)
-Base.:+(a::GF256, b::Bool) = +(promote(a, b)...)
-Base.:-(a::GF256, b::Bool) = -(promote(a, b)...)
 Base.:+(a::GF256, b::GF256) = GF256(xor(a.x, b.x))
 Base.:-(a::GF256, b::GF256) = a + b
-Base.:/(a::GF256, b::Bool) = /(promote(a, b)...)
-Base.:^(a::GF256, b::Integer) = b < 0 ? throw(DomainError(b, "^$b undefined")) : exprq(b * logrq(a) % 510)
-Base.:/(a::GF256, b::GF256) = iszero(b) ? throw(DivideError()) : iszero(a) ? a : exprq(logrq(a) - logrq(b) + 255)
-Base.:*(a::GF256, b::GF256) = iszero(a) || iszero(b) ? zero(a) : exprq(logrq(a)+logrq(b))
-Base.:*(a::Bool, b::GF256) = a ? b : zero(GF256)
-Base.:*(a::GF256, b::Bool) = b*a
+function Base.:/(a::GF256, b::GF256)
+    !iszero(b) || throw(DivideError())
+    iszero(a) ? a : RQ_OCT_EXP[RQ_OCT_LOG[a.x] - RQ_OCT_LOG[b.x] + 255 + 1]
+end
+Base.:*(a::GF256, b::GF256) = iszero(a) || iszero(b) ? zero(GF256) : RQ_OCT_EXP[RQ_OCT_LOG[a.x] + RQ_OCT_LOG[b.x] + 1]
+Base.sub_with_overflow(a::GF256, b::GF256) = (a-b, false)
+Base.add_with_overflow(a::GF256, b::GF256) = (a+b, false)
+
+# comparison
+Base.:<(a::GF256, b::GF256) = a.x < b.x
+Base.:<=(a::GF256, b::GF256) = a.x <= b.x
+Base.:>(a::GF256, b::GF256) = a.x > b.x
+Base.:>=(a::GF256, b::GF256) = a.x >= b.x
+Base.:(==)(a::GF256, b::GF256) = a.x == b.x
--- test/GF256_test.jl
+++ test/GF256_test.jl
@@ -1,21 +1,69 @@
-@test GF256(2) + GF256(10) - GF256(10) == GF256(2)
-@test GF256(2) * GF256(10) / GF256(10) == GF256(2)
-@test GF256(1) + GF256(2) * GF256(10) - GF256(10) - GF256(10) == GF256(21)
-@test GF256(13) * GF256(1) == GF256(13)
-@test GF256(13) / GF256(1) == GF256(13)
-@test GF256(0) * GF256(1) == GF256(0)
-@test GF256(0) / GF256(1) == GF256(0)
-@test [GF256(i) for i in 0:255].*zero(GF256) == zeros(GF256, 256)
-@test [GF256(i) for i in 0:255].*one(GF256) == [GF256(i) for i in 0:255]
-@test [GF256(i) for i in 0:255].*GF256(2) == [GF256(2)*GF256(i) for i in 0:255]
-@test GF256(11) + false == GF256(11)
-@test GF256(11) + true == GF256(11) + GF256(1)
-@test GF256(11) * false == GF256(0)
-@test GF256(11) * true == GF256(11)
-@test GF256(11) / true == GF256(11)
-@test GF256(123)^0 == GF256(1)
-@test GF256(123)^1 == GF256(123)
-@test GF256(123)^2 == GF256(123) * GF256(123)
-@test GF256(46)^3 == GF256(46) * GF256(46) * GF256(46)
-@test GF256(10)^20 == reduce(*, (GF256(10) for _ in 1:20))
-@test GF256(56)^30 == reduce(*, (GF256(56) for _ in 1:30))
+
+@testset "Properties" begin
+    @test iszero(GF256(0))
+    @test !iszero(GF256(1))
+    @test !iszero(GF256(255))
+    @test isone(GF256(1))
+    @test !isone(GF256(0))
+    @test !isone(GF256(255))
+    @test zero(GF256) == GF256(0)
+    @test zero(GF256) == zero(GF256(1))
+    @test one(GF256) == GF256(1)
+    @test one(GF256) == one(GF256(0))    
+end
+
+@testset "Logarithms and exponentiation" begin
+    a = GF256(0)
+    @test log(exp(a)) == a
+    for x in 1:254
+        a = GF256(x)
+        @test log(exp(a)) == a
+        @test exp(log(a)) == a                
+    end
+    a = GF256(255)
+    @test exp(log(a)) == a
+end
+
+@testset "Division and multiplication" begin
+    β = GF256(43) # chosen arbitrarily
+    @test β * zero(GF256) == zero(GF256)
+    @test β * one(GF256) == β
+    for x in 1:255
+        @test (β * GF256(x)) / GF256(x) == β
+        @test (β / GF256(x)) * GF256(x) == β
+    end
+end
+
+@testset "Addition and subtraction" begin
+    β = GF256(43) # chosen arbitrarily 
+    @test β + zero(GF256) == β
+    @test β - zero(GF256) == β
+    for x in 0:255
+        a = GF256(x)
+        @test iszero(a+a)
+        @test iszero(a-a)
+    end
+end
+
+# TODO: remove
+# @test GF256(2) + GF256(10) - GF256(10) == GF256(2)
+# @test GF256(2) * GF256(10) / GF256(10) == GF256(2)
+# @test GF256(1) + GF256(2) * GF256(10) - GF256(10) - GF256(10) == GF256(21)
+# @test GF256(13) * GF256(1) == GF256(13)
+# @test GF256(13) / GF256(1) == GF256(13)
+# @test GF256(0) * GF256(1) == GF256(0)
+# @test GF256(0) / GF256(1) == GF256(0)
+# @test [GF256(i) for i in 0:255].*zero(GF256) == zeros(GF256, 256)
+# @test [GF256(i) for i in 0:255].*one(GF256) == [GF256(i) for i in 0:255]
+# @test [GF256(i) for i in 0:255].*GF256(2) == [GF256(2)*GF256(i) for i in 0:255]
+# # @test GF256(11) + false == GF256(11)
+# # @test GF256(11) + true == GF256(11) + GF256(1)
+# # @test GF256(11) * false == GF256(0)
+# # @test GF256(11) * true == GF256(11)
+# # @test GF256(11) / true == GF256(11)
+# @test GF256(123)^0 == GF256(1)
+# @test GF256(123)^1 == GF256(123)
+# @test GF256(123)^2 == GF256(123) * GF256(123)
+# @test GF256(46)^3 == GF256(46) * GF256(46) * GF256(46)
+# @test GF256(10)^20 == reduce(*, (GF256(10) for _ in 1:20))
+# @test GF256(56)^30 == reduce(*, (GF256(56) for _ in 1:30))
